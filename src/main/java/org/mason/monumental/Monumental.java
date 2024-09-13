8dfc3c64f48ea32501a8ac5e55ab117f1944223b
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-09-13 16:38:35 -0700

    Init

---
+++ src/main/java/org/mason/monumental/Monumental.java
@@ -0,0 +1,10 @@
+package org.mason.monumental;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Monumental implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/mason/monumental/client/MonumentalClient.java
@@ -0,0 +1,10 @@
+package org.mason.monumental.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MonumentalClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
